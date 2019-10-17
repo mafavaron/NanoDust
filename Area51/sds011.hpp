657306fd195bf9c9e5cb782b8c94e64da9385ba4
Mauri Favaron <mafavaron@mac.com>
2019-10-17 23:32:28 +0200

    Update sds011.hpp
    In progress...

--- Area51/sds011.hpp
+++ Area51/sds011.hpp
@@ -3,10 +3,18 @@
 #define OUT_CHAR 20
 #define IN_CHAR  11
 
+// Reporting modes
+#define REPMODE_CONTINUOUS 0x00
+#define REPMODE_POLLING    0x01
+
 class sds011 {
 	private:
 		unsigned short int	id;
 		char				sCommand[OUT_CHAR];
 		char				sResponse[IN_CHAR];
 	public:
+		sds011(void);
+		~sds011(void);
+		void setReportingMode(const unsigned char cReportingMode);
+		unsigned char getReportingMode(void);
 };
