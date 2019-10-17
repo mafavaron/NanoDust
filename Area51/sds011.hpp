c530e84caeec1d0ed256fdf63433532c6d546758
Mauri Favaron <mafavaron@mac.com>
2019-10-17 23:12:07 +0200

    Create sds011.hpp

---
+++ Area51/sds011.hpp
@@ -0,0 +1,12 @@
+#include "serprt.hpp"
+
+#define OUT_CHAR 20
+#define IN_CHAR  11
+
+class sds011 {
+	private:
+		unsigned short int	id;
+		char				sCommand[OUT_CHAR];
+		char				sResponse[IN_CHAR];
+	public:
+};
