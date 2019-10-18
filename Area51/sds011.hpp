613fa5232cee89a0581f461742b7eb9846afc636
Mauri Favaron <mafavaron@mac.com>
2019-10-18 05:46:21 +0200

    Update sds011.hpp
    New interface hypothesis (not yet arranging error responses)

--- Area51/sds011.hpp
+++ Area51/sds011.hpp
@@ -3,10 +3,6 @@
 #define OUT_CHAR 20
 #define IN_CHAR  11
 
-// Reporting modes
-#define REPMODE_CONTINUOUS 0x00
-#define REPMODE_POLLING    0x01
-
 class sds011 {
 	private:
 		unsigned short int	id;
@@ -15,6 +11,11 @@ class sds011 {
 	public:
 		sds011(void);
 		~sds011(void);
-		void setReportingMode(const unsigned char cReportingMode);
-		unsigned char getReportingMode(void);
+		bool setContinuousMode(void);
+		bool setPollingMode(void);
+		bool getReportingMode(void);
+		bool getData(float* pm_2_5, float* pm_10);
+		bool setInScan(void);
+		bool setOutScan(void);
+		bool getScanState(void);
 };
