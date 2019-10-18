f66718b89758b3cc4d7d4ae10e0648a2fc800671
Mauri Favaron <mafavaron@mac.com>
2019-10-18 06:01:47 +0200

    Update sds011.hpp
    Interface type, final decision

--- Area51/sds011.hpp
+++ Area51/sds011.hpp
@@ -6,16 +6,18 @@
 class sds011 {
 	private:
 		unsigned short int	id;
-		char				sCommand[OUT_CHAR];
-		char				sResponse[IN_CHAR];
+		char				cvCommand[OUT_CHAR];
+		char				cvResponse[IN_CHAR];
+		unsigned short int	iRetCode;
+		string				sRetMsg;
 	public:
 		sds011(void);
 		~sds011(void);
-		bool setContinuousMode(void);
-		bool setPollingMode(void);
-		bool getReportingMode(void);
-		bool getData(float* pm_2_5, float* pm_10);
-		bool setInScan(void);
-		bool setOutScan(void);
-		bool getScanState(void);
+		short int setContinuousMode(void);					// 0:OK, -1:Error
+		short int setPollingMode(void);						// 0:OK, -1:Error
+		short int getReportingMode(void);					// 1:Polling, 0:Stream, -1:Error
+		short int getData(float* pm_2_5, float* pm_10);		// 0:OK, -1:Error
+		short int setInScan(void);							// 0:OK, -1:Error
+		short int setOutScan(void);							// 0:OK, -1:Error
+		short int getScanState(void);						// 1:In scan, 0:Out scan, -1:Error
 };
