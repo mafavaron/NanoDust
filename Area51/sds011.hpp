be4262e38810949f5d7c59ace81ef89d9a8879b8
Mauri Favaron <mafavaron@mac.com>
2019-10-18 06:15:58 +0200

    Update sds011.hpp
    Functional part of interface defined

--- Area51/sds011.hpp
+++ Area51/sds011.hpp
@@ -17,13 +17,18 @@ class sds011 {
 	public:
 		sds011(void);
 		~sds011(void);
-		short int setContinuousMode(void);					// 0:OK, -1:Error
-		short int setPollingMode(void);						// 0:OK, -1:Error
-		short int getReportingMode(void);					// 1:Polling, 0:Stream, -1:Error
-		short int getData(float* pm_2_5, float* pm_10);		// 0:OK, -1:Error
-		short int setInScan(void);							// 0:OK, -1:Error
-		short int setOutScan(void);							// 0:OK, -1:Error
-		short int getScanState(void);						// 1:In scan, 0:Out scan, -1:Error
+		short int setStreamMode(void);							// 0:OK, -1:Error
+		short int setPollingMode(void);							// 0:OK, -1:Error
+		short int getReportingMode(void);						// 1:Polling, 0:Stream, -1:Error
+		short int getData(float* pm_2_5, float* pm_10);			// 0:OK, -1:Error
+		short int setDeviceId(unsigned short int* id);			// 0:OK, -1:Error
+		short int getDeviceId(unsigned short int* id);			// 0:OK, -1:Error
+		short int setInScan(void);								// 0:OK, -1:Error
+		short int setOutScan(void);								// 0:OK, -1:Error
+		short int getScanState(void);							// 1:In scan, 0:Out scan, -1:Error
+		short int setStreamingPeriod(unsigned short int* dt);	// 0:OK, -1:Error
+		short int getStreamingPeriod(unsigned short int* dt);	// 0:OK, -1:Error
+		short int getFirmwareDate(unsigned int* date);			// 0:OK, -1:Error
 };
 
 #endif /* __SDS011__ */
