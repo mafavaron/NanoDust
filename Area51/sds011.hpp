8a94f8fe3d9caeb27a45e674109d54650b2722d9
Mauri Favaron <mafavaron@mac.com>
2019-10-19 06:52:10 +0200

    Update sds011.hpp
    Member added, containing serial port name

--- Area51/sds011.hpp
+++ Area51/sds011.hpp
@@ -8,31 +8,32 @@
 #define IN_CHAR  10
 
 class sds011 {
-	private:
-		unsigned short int	id;
-		unsigned char		cvCommand[OUT_CHAR];
-		unsigned char		cvResponse[IN_CHAR];
-		unsigned short int	iRetCode;
-		string				sRetMsg;
-	public:
-		sds011(void);
-		~sds011(void);
-		short int setStreamMode(void);							// 0:OK, -1:Error
-		short int setPollingMode(void);							// 0:OK, -1:Error
-		short int getReportingMode(void);						// 1:Polling, 0:Stream, -1:Error
-		short int getData(float* pm_2_5, float* pm_10);			// 0:OK, -1:Error
-		short int setDeviceId(unsigned short int* id);			// 0:OK, -1:Error
-		short int getDeviceId(unsigned short int* id);			// 0:OK, -1:Error
-		short int setInScan(void);								// 0:OK, -1:Error
-		short int setOutScan(void);								// 0:OK, -1:Error
-		short int getScanState(void);							// 1:In scan, 0:Out scan, -1:Error
-		short int setStreamingPeriod(unsigned short int* dt);	// 0:OK, -1:Error
-		short int getStreamingPeriod(unsigned short int* dt);	// 0:OK, -1:Error
-		short int getFirmwareDate(unsigned int* date);			// 0:OK, -1:Error
-		unsigned short int getCompletionCode(void);
-		string getCompletionMessage(void);
-	private:
-		unsigned char computeChecksum(void);
+private:
+    unsigned short int	id;
+    unsigned char		cvCommand[OUT_CHAR];
+    unsigned char		cvResponse[IN_CHAR];
+    unsigned short int	iRetCode;
+    string				sSerialPort;
+    string				sRetMsg;
+public:
+    sds011(void);
+    ~sds011(void);
+    short int setStreamMode(void);							// 0:OK, -1:Error
+    short int setPollingMode(void);							// 0:OK, -1:Error
+    short int getReportingMode(void);						// 1:Polling, 0:Stream, -1:Error
+    short int getData(float* pm_2_5, float* pm_10);			// 0:OK, -1:Error
+    short int setDeviceId(unsigned short int* id);			// 0:OK, -1:Error
+    short int getDeviceId(unsigned short int* id);			// 0:OK, -1:Error
+    short int setInScan(void);								// 0:OK, -1:Error
+    short int setOutScan(void);								// 0:OK, -1:Error
+    short int getScanState(void);							// 1:In scan, 0:Out scan, -1:Error
+    short int setStreamingPeriod(unsigned short int* dt);	// 0:OK, -1:Error
+    short int getStreamingPeriod(unsigned short int* dt);	// 0:OK, -1:Error
+    short int getFirmwareDate(unsigned int* date);			// 0:OK, -1:Error
+    unsigned short int getCompletionCode(void);
+    string getCompletionMessage(void);
+private:
+    unsigned char computeChecksum(void);
 };
 
 #endif /* __SDS011__ */
