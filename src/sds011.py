7cff3b24a02a16e795a7f6f4dd021fd4841bb57b
Mauri Favaron <mafavaron@mac.com>
2019-10-21 21:16:57 +0200

    Update sds011.py
    Corrections

--- src/sds011.py
+++ src/sds011.py
@@ -29,10 +29,10 @@ class SDS011:
 		self.byte = 0
 		self.data = ""
 
-	def dump(d, prefix=''):
+	def dump(self, d, prefix=''):
 		print(prefix + ' '.join(x.encode('hex') for x in d))
 
-	def construct_command(cmd, data=[]):
+	def construct_command(self, cmd, data=[]):
 		assert len(data) <= 12
 		data += [0,]*(12-len(data))
 		checksum = (sum(data)+cmd-2)%256
@@ -44,7 +44,7 @@ class SDS011:
 			dump(ret, '> ')
 		return ret
 
-	def process_data(d):
+	def process_data(self, d):
 		r = struct.unpack('<HHxxBB', d[2:])
 		pm25 = r[0]/10.0
 		pm10 = r[1]/10.0
@@ -52,7 +52,7 @@ class SDS011:
 		return [pm25, pm10]
 		#print("PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
 
-	def process_version(d):
+	def process_version(self, d):
 		r = struct.unpack('<BBBHBB', d[3:])
 		checksum = sum(ord(v) for v in d[2:8])%256
 		print("Y: {}, M: {}, D: {}, ID: {}, CRC={}".format(r[0], r[1], r[2], hex(r[3]), "OK" if (checksum==r[4] and r[5]==0xab) else "NOK"))
@@ -82,21 +82,21 @@ class SDS011:
 
 	def cmd_set_sleep(self, sleep):
 		mode = 0 if sleep else 1
-		self.ser.write(construct_command(CMD_SLEEP, [0x1, mode]))
-		read_response()
+		self.ser.write(self.construct_command(self.CMD_SLEEP, [0x1, mode]))
+		self.read_response()
 
 	def cmd_set_working_period(self, period):
-		self.ser.write(construct_command(CMD_WORKING_PERIOD, [0x1, period]))
-		read_response()
+		self.ser.write(self.construct_command(CMD_WORKING_PERIOD, [0x1, period]))
+		self.read_response()
 
 	def cmd_firmware_ver(self):
-		self.ser.write(construct_command(CMD_FIRMWARE))
-		d = read_response()
-		process_version(d)
+		self.ser.write(self.construct_command(CMD_FIRMWARE))
+		d = self.read_response()
+		self.process_version(d)
 
 	def cmd_set_id(self, id):
 		id_h = (id>>8) % 256
 		id_l = id % 256
-		self.ser.write(construct_command(CMD_DEVICE_ID, [0]*10+[id_l, id_h]))
-		read_response()
+		self.ser.write(self.construct_command(CMD_DEVICE_ID, [0]*10+[id_l, id_h]))
+		self.read_response()
 
